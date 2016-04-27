0b9e98d8292bb487e8e8963b9c6d4197196e3c54
Holstad <ola.holstad@gmail.com>
2016-04-27 09:44:33 -0700

    world

--- mario2/core/src/se/jolo/game/mario/MarioBros.java
+++ mario2/core/src/se/jolo/game/mario/MarioBros.java
@@ -1,27 +1,27 @@
 package se.jolo.game.mario;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MarioBros extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import se.jolo.game.mario.screens.PlayScreen;
+
+public class MarioBros extends Game {
+	public SpriteBatch batch;
+    public static final int V_WIDTH = 400;
+    public static final int V_HEIGHT = 208;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 }
---
+++ mario2/core/src/se/jolo/game/mario/screens/PlayScreen.java
@@ -0,0 +1,97 @@
+package se.jolo.game.mario.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import se.jolo.game.mario.MarioBros;
+import se.jolo.game.mario.scenes.Hud;
+
+/**
+ * Created by Holstad on 26/04/16.
+ */
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    private OrthographicCamera camera;
+    private Viewport gamePort;
+    private Hud hud;
+
+    private TmxMapLoader tmxMapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    public PlayScreen (MarioBros game){
+        this.game = game;
+        camera = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, camera);
+        hud = new Hud(game.batch);
+        tmxMapLoader = new TmxMapLoader();
+        map = tmxMapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        camera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+    }
+
+    public void update (float dt){
+        handleInput(dt);
+        camera.update();
+        renderer.setView(camera);
+    }
+
+    private void handleInput(float dt) {
+        if(Gdx.input.isTouched()){
+            camera.position.x += 100 * dt;
+        }
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.render();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
